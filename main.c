b833a47ff792368e990efed5df3e1652a8d0a1f7
Марк <Leganyst@yandex.ru>
2023-12-22 02:09:40 +0300

    2.0

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <locale.h>
+#include <wchar.h>
 
 /*
 В файле clients.txt содержатся записи о клиентах. Первым идут ФИО клиента, через запятую номер, 
@@ -16,7 +17,6 @@ Cвязь внутри сети|   1 | 0.30 |     мин          |
 Номер телефона | код услуги | дата | длительность использования в секундах |
 
 */
-
 typedef struct {
     int day, month, year, hour, minute, seconds;
 } tDateTime;
@@ -28,20 +28,27 @@ typedef struct {
 } tPhoneCall;
 
 
+typedef struct {
+    wchar_t name[300];
+    int code;
+    float price;
+    wchar_t time[300];
+} tRates;
+
 int isTimeInInterval(const tDateTime *time, const tDateTime *start, const tDateTime *end) {
     if (time->year < start->year || (time->year == start->year && time->month < start->month) ||
         (time->year == start->year && time->month == start->month && time->day < start->day) ||
         (time->year == start->year && time->month == start->month && time->day == start->day &&
-         (time->hour < start->hour || (time->hour == start->hour && time->minute < start->minute) ||
-          (time->hour == start->hour && time->minute == start->minute && time->seconds < start->seconds)))) {
+        (time->hour < start->hour || (time->hour == start->hour && time->minute < start->minute) ||
+        (time->hour == start->hour && time->minute == start->minute && time->seconds < start->seconds)))) {
         return 0;
     }
 
     if (time->year > end->year || (time->year == end->year && time->month > end->month) ||
         (time->year == end->year && time->month == end->month && time->day > end->day) ||
         (time->year == end->year && time->month == end->month && time->day == end->day &&
-         (time->hour > end->hour || (time->hour == end->hour && time->minute > end->minute) ||
-          (time->hour == end->hour && time->minute == end->minute && time->seconds > end->seconds)))) {
+        (time->hour > end->hour || (time->hour == end->hour && time->minute > end->minute) ||
+        (time->hour == end->hour && time->minute == end->minute && time->seconds > end->seconds)))) {
         return 0;
     }
 
@@ -49,26 +56,32 @@ int isTimeInInterval(const tDateTime *time, const tDateTime *start, const tDateT
 }
 
 
-float calculateSummCost(const tPhoneCall *call) {
-    int code = call->code_service;
-    switch (code) {
-        case 1:
-            return 0.30 / 60 * call->duration_in_seconds;
-        case 3:
-            return 0.50 / 60 * call->duration_in_seconds;
-        case 4:
-            return 10.0 / 60 * call->duration_in_seconds;
-        case 5:
-            return 50.0 / 60 * call->duration_in_seconds;
-        default:
-        return 0.0;
+float calculateSummCost(const tPhoneCall *call, const tRates *rate) {
+  int code = call->code_service;
+  if (wcscmp(rate->time, L"мин") == 0){
+    return call->duration_in_seconds * (rate->price / 60);
+  } else if (wcscmp(rate->time, L"сутки") == 0) {
+    return call->duration_in_seconds * (rate->price / (24 * 3600));
+  } else if (wcscmp(rate->time, L"месяц") == 0) {
+    return call->duration_in_seconds * (rate->price / (30 * 24 * 3600));
+  }
+}
+
+
+void readRates(tRates *rate, FILE *rates) {
+    wchar_t buffer_rate[300];
+    while(fgetws(buffer_rate, 300, rates)) {
+        fwscanf(rates, L"%99[^,], %d, %f, %99[^\n]", rate->name, &rate->code, &rate->price, rate->time);
+        // fwscanf(rates, L"%ls, %d, %f, %ls", rate->name, &rate->code, &rate->price, rate->time);
+        if (wcscmp(rate->name, L"Междугородние звонки") == 0) {
+            break;
+        }
     }
 }
 
 
 int main(void) {
-
-    setlocale(LC_ALL, "ru");
+    setlocale(LC_ALL, "Russian");
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
 
@@ -81,6 +94,7 @@ int main(void) {
     }
 
     char buffer[100];
+ 
     while (fgets(buffer, 100, params)) {
         
         tDateTime start_time;
@@ -88,6 +102,7 @@ int main(void) {
         tPhoneCall call;
         float result_summ = 0;
         int count_calls = 0;
+        int is_search = 1;
 
         sscanf(buffer, "%d.%d.%d %d:%d:%d %d.%d.%d %d:%d:%d",
                &start_time.day, &start_time.month, &start_time.year,
@@ -109,20 +124,39 @@ int main(void) {
                    &call.call_time.hour, &call.call_time.minute, &call.call_time.seconds,
                    &call.duration_in_seconds);
 
-            if (call.code_service != 2) {
-                if (isTimeInInterval(&call.call_time, &start_time, &end_time)) {
-                    result_summ += calculateSummCost(&call);
+            if (isTimeInInterval(&call.call_time, &start_time, &end_time)) {
+                FILE *rates = _wfopen(L"rates.txt", L"r, ccs=UTF-8");
+                if (rates == NULL) {
+                    printf("Error opening file");
+                    return 1;
+                }
+                tRates rate;
+                readRates(&rate, rates);
+
+                if (call.code_service == rate.code) {
+                    result_summ += calculateSummCost(&call, &rate);
                     count_calls++;
-                    
-                    printf("Phone Number: %lld, Code Service: %d, Duration: %d seconds\n",
-                           call.phone_number, call.code_service, call.duration_in_seconds);
+                    is_search = 0;
+                    // printf("Phone Number: %lld, Code Service: %d, Duration: %d seconds\n",
+                            // call.phone_number, call.code_service, call.duration_in_seconds);
                 }
             }
+
         }
         fprintf(report, "%d, %2.2f, %02d.%02d.%02d %02d:%02d:%02d, %02d.%02d.%02d %02d:%02d:%02d\n", count_calls, result_summ,
                 start_time.day, start_time.month, start_time.year, start_time.hour, start_time.minute, start_time.seconds,
                 end_time.day, end_time.month, end_time.year, end_time.hour, end_time.minute, end_time.seconds);
         fclose(info_services);
+        if (is_search) {
+            wprintf(L"%02d.%02d.%02d %02d:%02d:%02d - %02d.%02d.%02d %02d:%02d:%02d - Нет данных\n", 
+            start_time.day, start_time.month, start_time.year, start_time.hour, start_time.minute, start_time.seconds,
+            end_time.day, end_time.month, end_time.year, end_time.hour, end_time.minute, end_time.seconds);
+        } else {
+            wprintf(L"%02d.%02d.%02d %02d:%02d:%02d - %02d.%02d.%02d %02d:%02d:%02d - Данные проанализированы. Результаты выведены в файл Report.txt\n",
+            start_time.day, start_time.month, start_time.year, start_time.hour, start_time.minute, start_time.seconds,
+            end_time.day, end_time.month, end_time.year, end_time.hour, end_time.minute, end_time.seconds);
+        }
+
     }
 
 
